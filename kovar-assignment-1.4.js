6878eed01396ee461b48b0ab1d26ebf443209fcf
Sarah Kovar <sarahkovar@gmail.com>
2019-12-01 21:29:43 -0600

    Adding second commit

---
+++ kovar-assignment-1.4.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Sarah Kovar
+; Date: 1 December 2019
+; Description: This program demonstrates the
+; use of primitive data types in JavaScript.
+;===========================================
+*/ 
+
+//defining variables
+var firstName = "Sarah";
+var lastName = "Kovar";
+var age = "38";
+var street = "123 S 4th Street";
+var city = "New Orleans";
+var zipCode = "24601";
+
+//display variables
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
